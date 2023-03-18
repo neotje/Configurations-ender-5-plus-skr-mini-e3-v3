dd02d9292a9ea054dbb9ffecdda901df54c19b35
Neo Hop <neotje111@gmail.com>
2023-03-18 22:25:39 +0100

    default probe offset closer to stock offset
    Note! Still a good idea to run the z probe offset wizard.

--- config/examples/Creality/Ender-5 Plus/BigTreeTech SKR Mini E3 V3.0/Configuration.h
+++ config/examples/Creality/Ender-5 Plus/BigTreeTech SKR Mini E3 V3.0/Configuration.h
@@ -1494,7 +1494,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -44, -5, -1.7 }
+#define NOZZLE_TO_PROBE_OFFSET { -44, -5, -2.65 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
